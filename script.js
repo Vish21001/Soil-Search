5930d741b5916e0e39d99dcfb891758f39e746e6
Vish21001 <agarwalvishisht@gmail.com>
2025-09-15 01:24:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+// Button for About Us Page
+window.onload = function () {
 
+	const aboutus =document.getElementById("about-us")
+	if (aboutus){ 
+		aboutus.addEventListener("click",function() {
+		window.location.href = "/about-us"
+	});
+
+	}
+
+
+
+};
+// Button for Soil AI Page
+window.onload = function () {
+
+
+
+const aboutus =document.getElementById("soil-ai")
+	if (aboutus){ 
+		aboutus.addEventListener("click",function() {
+		window.location.href = "/soil-ai"
+	});
+
+	}
+
+
+
+};
+// Button for Common Plants Page
+window.onload = function () {
+
+	const aboutus =document.getElementById("common-plants")
+	if (aboutus){ 
+		aboutus.addEventListener("click",function() {
+		window.location.href = "/common-plants"
+	});
+
+	}
+
+
+
+};
